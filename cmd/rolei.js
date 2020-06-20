f791202fa08e4dfc9295e9dab5b95c8679b5fb0f
osamagmt <34580348+osamagmt@users.noreply.github.com>
2020-06-20 21:53:44 +0100

    Update rolei.js

--- cmd/rolei.js
+++ cmd/rolei.js
@@ -3,23 +3,23 @@ module.exports.run = async (client,M,args) => {
     if (M.member.hasPermission('MANAGE_MESSAGES')){
   let subtext = M.content.split(/\s+/g).slice(1).join(" ");
     let role = M.mentions.roles.first();
-	if (role) {
-		var RichEmbed = new Discord.RichEmbed()
-		.setTitle(`Info Role : ${role.name}`)
-		.setThumbnail(M.guild.iconURL)
-		.setTimestamp()
-		.setColor(`${role.hexColor}`)
-		.setFooter(`Requested by: ${M.member.displayName}`, `${M.author.avatarURL}`)
-		.addField(`**لون الرتبه:**`, `${role.hexColor}`)
-		.addField(`**ايدي الرتبه:**`, `${role.id}`)
-		.addField(`**ترتيب الرتبه:**`, `${role.position}`)
-		.addField(`**اعضاء الرتبة :**`, `${role.members.map(m => m.mention.displayName).join (` ,\n`)}`, true)
-		.addField(`**تاريخ انشاء الرتبه:**`, `${role.createdAt}`)
-		M.channel.send({embed: RichEmbed});
-	}
-	if (!role) {
-		M.channel.send(`either that wasn't a role in this server, or the name is incorrect`);
-	}
+    if (role) {
+        var RichEmbed = new Discord.RichEmbed()
+        .setTitle(`Info Role : ${role.name}`)
+        .setThumbnail(M.guild.iconURL)
+        .setTimestamp()
+        .setColor(`${role.hexColor}`)
+        .setFooter(`Requested by: ${M.member.displayName}`, `${M.author.avatarURL}`)
+        .addField(`**لون الرتبه:**`, `${role.hexColor}`)
+        .addField(`**ايدي الرتبه:**`, `${role.id}`)
+        .addField(`**ترتيب الرتبه:**`, `${role.position}`)
+        .addField(`**اعضاء الرتبة :**`, `${role.members.map(m => m.user.username).join (` ,\n`)}`, true)
+        .addField(`**تاريخ انشاء الرتبه:**`, `${role.createdAt}`)
+        M.channel.send({embed: RichEmbed});
+    }
+    if (!role) {
+        M.channel.send(`either that wasn't a role in this server, or the name is incorrect`);
+    }
 }
 }
 module.exports.help = {
