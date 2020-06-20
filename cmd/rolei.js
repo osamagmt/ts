88d42b5d9c53d56efb50fe5ed8f200ef57fbd501
osamagmt <34580348+osamagmt@users.noreply.github.com>
2020-06-20 22:07:56 +0100

    Update rolei.js

--- cmd/rolei.js
+++ cmd/rolei.js
@@ -4,18 +4,8 @@ module.exports.run = async (client,M,args) => {
   let subtext = M.content.split(/\s+/g).slice(1).join(" ");
     let role = M.mentions.roles.first();
     if (role) {
-        var RichEmbed = new Discord.RichEmbed()
-        .setTitle(`Info Role : ${role.name}`)
-        .setThumbnail(M.guild.iconURL)
-        .setTimestamp()
-        .setColor(`${role.hexColor}`)
-        .setFooter(`Requested by: ${M.member.displayName}`, `${M.author.avatarURL}`)
-        .addField(`**لون الرتبه:**`, `${role.hexColor}`)
-        .addField(`**ايدي الرتبه:**`, `${role.id}`)
-        .addField(`**ترتيب الرتبه:**`, `${role.position}`)
-        .addField(`**اعضاء الرتبة :**`, `${role.members.map(m => m.user).join (` ,\n`)}`, true)
-        .addField(`**تاريخ انشاء الرتبه:**`, `${role.createdAt}`)
-        M.channel.send({embed: RichEmbed});
+
+        M.channel.send(`Info Role : ${role.name}  \n **لون الرتبه:**${role.hexColor}  \n  **ايدي الرتبه:**${role.id}   \n **اعضاء الرتبة :**${role.members.map(m => m.user).join (` ,\n`)} \n **تاريخ انشاء الرتبه:**${role.createdAt}`);
     }
     if (!role) {
         M.channel.send(`either that wasn't a role in this server, or the name is incorrect`);
