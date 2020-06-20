1d6636e3782bf396c9c59eebbeaf4cbf2cce6e7f
osamagmt <34580348+osamagmt@users.noreply.github.com>
2020-06-20 02:02:31 +0100

    Update setavatar.js

--- cmd/setavatar.js
+++ cmd/setavatar.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const VIP = ["460976885036220426","410421617869455370","474200581163057153",process.env.BOT_ID];
+const VIP = ["460976885036220426","563646438408716298","",process.env.BOT_ID];
 
 module.exports.run = async (client,message,args) => {
   if (!VIP.includes(message.author.id)) return;
