dd1fbae5eac1ae0c2e6a66bdd0423f59d37efce2
osamagmt <34580348+osamagmt@users.noreply.github.com>
2020-09-14 09:31:47 +0100

    Create Rolein.js

---
+++ Rolein.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+module.exports.run = async (client,M,args) => {
+    if (M.member.hasPermission('MANAGE_MESSAGES')){
+  let subtext = M.content.split(/\s+/g).slice(1).join(" ");
+    let role = M.mentions.roles.first();
+    if (role) {
+        var RichEmbed = new Discord.RichEmbed()
+        .setTitle(`Info Role : ${role.name}`)
+        .setThumbnail(M.guild.iconURL)
+        .setTimestamp()
+        .setColor(`${role.hexColor}`)
+        .setFooter(`Requested by: ${M.member.displayName}`, `${M.author.avatarURL}`)
+        .addField(`**لون الرتبه:**`, `${role.hexColor}`)
+        .addField(`**ايدي الرتبه:**`, `${role.id}`)
+        .addField(`**ترتيب الرتبه:**`, `${role.position}`)
+        .addField(`**اعضاء الرتبة :**`, `${role.members.map(m => m.user).join (` ,\n`)}`, true)
+        .addField(`**تاريخ انشاء الرتبه:**`, `${role.createdAt}`)
+        M.channel.send({embed: RichEmbed});
+    }
+    if (!role) {
+        M.channel.send(`either that wasn't a role in this server, or the name is incorrect`);
+    }
+}
+}
+module.exports.help = {
+    name: "rolei"
+}
+ 
